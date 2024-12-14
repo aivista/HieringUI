ee93f752367f5b37530af0c6ba300698a4067bb1
Manas Das <dasmanas2308@gmail.com>
2024-12-14 22:02:29 +0530

    candidate-info logic change dinamically

--- src/app/hiring-manager/components/pages/job-details/candidate-info/candidate-info.component.ts
+++ src/app/hiring-manager/components/pages/job-details/candidate-info/candidate-info.component.ts
@@ -9,12 +9,34 @@ export class CandidateInfoComponent {
   @Input() candidate: any;
   @Output() close = new EventEmitter<void>();
 
+  isInterviewScheduled: boolean = true;
+  isSelected : boolean = false;
+
   closeModal() {
     this.close.emit();
+    
   }
 
-  ngOnInit(){
-    console.log('Hello World',this.candidate);
+  candidateStatus: string = '';
+
+  ngOnInit() {
+    console.log('Hello World', this.candidate);
+
+    // Store the status in a variable
+    if (this.candidate && this.candidate.status) {
+      this.candidateStatus = this.candidate.status;
+      console.log('Candidate Status:', this.candidateStatus);
+    }
+    if (this.candidateStatus === 'Interview Scheduled') {
+
+       this.isInterviewScheduled = true;
+       this.isSelected = false;
+    } else {
+      this.isInterviewScheduled = false;
+      this.isSelected = true;
+    }
+    // this.isInterviewScheduled = this.candidateStatus !== 'Interview Complited';
   }
+
   
 }

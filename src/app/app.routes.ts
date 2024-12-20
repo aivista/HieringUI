d09d0366f491f4568e9fe608a13546d0d9b43218
Tohidul Islam <tohidulislam4599@gmail.com>
2024-12-20 19:44:37 +0530

    privios code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,14 +23,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'Manager',
+    path: '',
     loadChildren: () =>
       import('./hiring-manager/hiring-manager.module').then(
         (m) => m.HiringManagerModule
       ),
   },
   {
-    path: '',
+    path: 'cnadidate',
     loadChildren: () =>
       import('./candidate/candidate.module').then((m) => m.CandidateModule),
   },
